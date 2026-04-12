ab7700d9025f2a93f853e353f2e1ccf4715675f0
victormosqueraconejo <victormanuelmosqueraconejo@gmail.com>
2026-04-11 22:50:05 -0500

    Correciones de dependecias completadas KAPT

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -34,6 +35,10 @@ android {
     buildFeatures {
         compose = true
     }
+}
+
+kotlin {
+    jvmToolchain(11)
 }
 
 dependencies {
